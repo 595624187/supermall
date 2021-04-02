4de40f1d6ba20f1980bf3a2c87919017a090d833
Administrator <595624187@qq.com>
2021-04-02 17:21:15 +0800

    HomeSwiper模块完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import {getHomeMultidata} from 'network/home'
+
 export default {
-  name: "Home"
+  name: "Home",
+  components:{
+    NavBar,
+    HomeSwiper
+  },
+  data(){
+    return{
+      banner:[],
+      recommend:[],
+      keywords:[],
+      dKeyword:[],
+    }
+  },
+  created(){
+    //1.请求多个数据
+    getHomeMultidata().then(res=>{
+      console.log(res)
+      this.banner = res.data.banner.list
+      this.recommend = res.data.recommend.list
+      this.keywords = res.data.keywords.list
+      this.dKeyword = res.data.dKeyword.list
+    })
+    // console.log(this.result)//不能打印，因为上面函数是异步函数
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color:var(--color-tint);
+  color:#fff;
+}
 </style>
